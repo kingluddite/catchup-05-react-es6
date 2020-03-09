186848c6b850889b87c5c37d46ec7cbc28eac284
Kingluddite <howley.phil@gmail.com>
2020-03-09 15:57:27 -0700

    Add arrow func question

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-// Example 1
-// We've seen a few ways to define functions:
-// slightly different but largely function similarly
+// Turn the following into arrow functions:
 
-function foo() {
-  console.log('foo');
+function subtract(num1, num2, num3) {
+  return num1 - num2 - num3;
 }
-
-var bar = function() {
-  console.log('bar');
-};
-
-const baz = function() {
-  console.log('baz');
-};
-
-// Example 2
-
-// Now we're going to learn a short-hand that has one glaring and very important difference. This is called an arrow function.
-
-// Let's convert this add function:
-
-// Start code
-// const add = function(num1, num2) {
-//   return num1 + num2;
-// };
-
-// First we replace the function keyword with an arrow:
-
-// const add = (num1, num2) => {
-//   return num1 + num2;
-// };
-
-// Example 3 - Refactor even more
-// If our function just has one line of logic that we want to return we can eliminate the return keyword and curly braces!
-// const add = (num1, num2) => num1 + num2;
-
-// Next
-// $ git checkout 08-arrow-func-activity-start
+function subtractWithOptionalThird(num1, num2, num3) {
+  if (num3) {
+    return num1 - num2 - num3;
+  } else {
+    return num1 - num2;
+  }
+}
+function sayHi(name) {
+  console.log('Hi ' + name + '!');
+}
