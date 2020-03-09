aebf4198c18a9480ebe08c15f64a1acab53c5b2f
Kingluddite <howley.phil@gmail.com>
2020-03-09 15:47:55 -0700

    Add start for activity

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-// Example 1
-function Dog(name, age) {
-  this.name = name;
-  this.age = age;
-  this.bark = function() {
-    console.log('yip yip! my name is ' + this.name);
-  };
-}
+// Using the class syntax creates a constructor function for Student that assigns a name, mathGrade, scienceGrade, englishGrade, and studentId to each student.
 
-var ralph = new Dog('Ralph', 12);
-ralph.bark();
+// Include a method that returns the GPA (the sum of the 3 grades divided by 3). (10 minutes)
 
-// Example 2
-// Well, there's another way to write this, using the class keyword
-// This method of creating constructor functions is slightly prefered as it provides some nifty functionality and protection against someone calling your function without the new keyword
-
-class Dog {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  bark() {
-    console.log('yip yip! my name is ' + this.name);
-  }
-}
-
-var ralph = new Dog('Ralph', 12);
-ralph.bark();
+## Solution
+`$ git checkout 06-constructor-activity-solution`
