3af824fabdfcffa3f14f14bec3d753ae9640bf54
Kingluddite <howley.phil@gmail.com>
2020-03-09 16:41:22 -0700

    update code for scope

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // Example 1
 // ## This works as we expect
-// let varOne = 'Hello from the global scope';
-
-// if (true) {
-//   console.log(varOne);
-// }
-// Output: Hello from the global scope
-
-// Example 2
-// ## This causes a problem
 let varOne = 'Hello from the global scope';
 
 if (true) {
   console.log(varOne);
-  let varTwo = 'Hello from the local scope';
 }
+// Output: Hello from the global scope
+
+// Example 2
+// ## This causes a problem
+// let varOne = 'Hello from the global scope';
+
+// if (true) {
+//   console.log(varOne);
+//   let varTwo = 'Hello from the local scope';
+// }
 
 // console.log(varTwo);
 // ERROR - varTwo is not defined
 
 // Example 3
 // ### Important scope rule
-let varOne = 'Hello from the global scope';
+// let varOne = 'Hello from the global scope';
 
-if (true) {
-  console.log(varOne);
-  let varTwo = 'Hello from the local scope';
-  console.log(varTwo);
-}
+// if (true) {
+//   console.log(varOne);
+//   let varTwo = 'Hello from the local scope';
+//   console.log(varTwo);
+// }
 
-console.log(varTwo);
+// console.log(varTwo);
 
 // This explains why we CAN access `varOne` and `varTwo` from _inside_ the "code block"
 //  But we CAN NOT access `varTwo` from _outside_ the "code block"
@@ -37,23 +37,23 @@ console.log(varTwo);
 // Example 4
 // ## Fun with "scope trees"
 // ### Let's draw a scope tree for the following:
-let varOne = 'varOne';
+// let varOne = 'varOne';
 
-if (true) {
-  console.log(varOne);
-  let varTwo = "Hello from varTwo's local scope";
-  console.log(varTwo);
+// if (true) {
+//   console.log(varOne);
+//   let varTwo = "Hello from varTwo's local scope";
+//   console.log(varTwo);
 
-  if (true) {
-    let varFour = "Hello from varFour's local scope";
-  }
-}
+//   if (true) {
+//     let varFour = "Hello from varFour's local scope";
+//   }
+// }
 
-if (true) {
-  let varThree = "Hello from varThree's local scope";
-}
+// if (true) {
+//   let varThree = "Hello from varThree's local scope";
+// }
 
-console.log(varTwo);
+// console.log(varTwo);
 
 // here is the scope tree for the above code
 
