32ff75c6a9b43f0284e7ace64807c61a420cd06a
Kingluddite <howley.phil@gmail.com>
2020-03-09 15:48:48 -0700

    Add constructor solutition

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
 
 // Include a method that returns the GPA (the sum of the 3 grades divided by 3). (10 minutes)
 
-## Solution
-`$ git checkout 06-constructor-activity-solution`
+// Solution
+class Student {
+  constructor(name, id, scienceGrade, englishGrade, mathGrade) {
+    this.name = name;
+    this.id = id;
+    this.scienceGrade = scienceGrade;
+    this.mathGrade = mathGrade;
+    this.englishGrade = englishGrade;
+  }
+
+  getGPA() {
+    return (this.scienceGrade + this.mathGrade + this.englishGrade) / 3;
+  }
+}
+
+var ralph = new Student('Ralph', 1200, 3.0, 4.0, 3.2);
+console.log(ralph.getGPA());
